d87cf966859c75018468b0cea64da3d1055b8e26
Sony Nguyen <sirarseus@gmail.com>
2015-07-18 18:54:17 +0200

    Fix C++: remove pointer buffer

--- core/core.cpp
+++ core/core.cpp
@@ -7,12 +7,10 @@ Core::Core(QObject *parent) : QObject(parent)
 
 Interval *Core::interval()
 {
-    Interval *buffer = new Interval;
-    return buffer;
+    return new Interval;
 }
 
 Interval *Core::interval(double min, double max)
 {
-    Interval *buffer = new Interval(min, max);
-    return buffer;
+    return new Interval(min, max);
 }
