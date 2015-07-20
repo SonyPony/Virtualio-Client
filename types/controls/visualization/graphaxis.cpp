503c1bafdcd29b71fc011e8e4c3e6175f752766a
Sony Nguyen <sirarseus@gmail.com>
2015-07-20 21:47:05 +0200

    Add C++: universal painting method for both directions - GraphAxis

--- types/controls/visualization/graphaxis.cpp
+++ types/controls/visualization/graphaxis.cpp
@@ -3,65 +3,112 @@
 #include <QPainter>
 #include <QVector>
 
+
 GraphAxis::GraphAxis(): AbstractGraphAxis()
 {
     m_lineWidth = 1;
     m_font = "Arial";
     m_fontSize = 10;
+    m_ratio = NULL;
+    setAcceptedMouseButtons(Qt::AllButtons);
+
+    connect(this, SIGNAL(enabledDirectionsChanged(int)), this, SLOT(setRatioBinding(int)));
+    connect(this, SIGNAL(fontSizeChanged(int)), this, SLOT(calculateOffset()));
+    connect(this, SIGNAL(valuesChanged(Interval*)), this, SLOT(calculateOffset()));
+    connect(this, SIGNAL(enabledDirectionsChanged(int)), this, SLOT(calculateOffset()));
 }
 
-void GraphAxis::paintHorizontalAxis(QPainter *painter)
+GraphAxis::~GraphAxis()
 {
-    int offset = 10;
-    int count = calculateValuesCount(m_verticalRatio, m_valuesCount);
-
-    //setting painter for drawing text
-    painter->setFont(font);
-    painter->setPen(QPen(m_textColor));
-}
-
-void GraphAxis::paintVerticalAxis(QPainter *painter)
-{
-    //it is for font metrics
-    QString intervalMin = QString::number(m_values->min(), 'f', 1);
-    QString intervalMax = QString::number(m_values->max(), 'f', 1);
-    QFont font(m_font, m_fontSize);
-    QFontMetrics fm(font);
-    QVector<QLineF> axisLines;
-    QSize content;
-    int offset = 10;
-    int count = calculateValuesCount(m_verticalRatio, m_valuesCount);
-    double piece = (height() - lineWidth()) / (count - 1);
-    int textWidth = (intervalMin.length() > intervalMax.length())
-                    ?fm.width(intervalMin)
-                    :fm.width(intervalMax);
-    content.setWidth(width() - textWidth);
-
-    //setting painter for drawing text
-    painter->setFont(font);
-    painter->setPen(QPen(m_textColor));
-
-    //first calculating vertical axis
-    for(int i = 0; i < count; i++) {
-        int y = piece * i ;//+ m_lineWidth / 2;// + m_verticalRatio - 1;
-        QLineF line(width() - content.width() + offset, y, width(), y);
-        axisLines.append(line);
-        if(i != 0 && i < count - 1)
-        painter->drawText(QPoint(0, y + m_fontSize / 2), m_labels[count - i - 1]);
-    }
-
-    //set color for drawing lines
-    painter->setPen(QPen(m_color,
-                         m_lineWidth));
-    painter->drawLines(axisLines);
+    m_ratio = NULL;
 }
 
 void GraphAxis::paint(QPainter *painter)
 {
-    if(m_enabledDirections & GraphAxis::Vertical)
-        paintVerticalAxis(painter);
+    GraphPaintProperties p;
+    QFont font(m_font, m_fontSize);
+    p.count = calculateValuesCount(*m_ratio, m_valuesCount);
+
+    if(m_enabledDirections & GraphAxis::Vertical) {
+        p.piece = (height() - m_lineWidth) / (p.count - 1);
+        p.calculateLinePos = [&](int i) -> QLineF {
+            return QLineF(m_offset, p.piece * i, width(), p.piece * i);
+        };
+        p.calculateTextPos = [&](int i) -> QPointF {
+            return QPointF(0, p.piece * i + m_fontSize / 2);
+        };
+        p.getText = [&](int i) -> QString {
+            return m_labels[p.count - i - 1];
+        };
+
+    }
+    else if(m_enabledDirections & GraphAxis::Horizontal) {
+        p.piece = (width() - m_lineWidth) / (p.count - 1);
+        p.calculateLinePos = [&](int i) -> QLineF {
+            return QLineF(p.piece * i, 0, p.piece * i, height() - m_offset);
+        };
+        p.calculateTextPos = [&](int i) -> QPointF {
+            QFontMetrics fm(font);
+            return QPointF(p.piece * i - fm.width(m_labels[p.count - i - 1]) / 2, height());
+        };
+        p.getText = [&](int i) -> QString {
+            return m_labels[i];
+        };
+    }
+
+    QVector<QLineF> axisLines;
+
+    //setting painter for drawing text
+    painter->setFont(font);
+    painter->setPen(QPen(m_textColor));
+
+    for(int i = 0; i < p.count; i++) {
+        axisLines.append(p.calculateLinePos(i));
+        if(i > 0 && i < p.count - 1)    //do not draw first and last
+            painter->drawText(p.calculateTextPos(i), p.getText(i));
+    }
+
+    painter->setPen(QPen(m_color, m_lineWidth));
+    painter->drawLines(axisLines);
+}
+
+void GraphAxis::wheelEvent(QWheelEvent *e)
+{
+    if(e->angleDelta().y() > 0)
+        setRatio(*m_ratio + 1);
+    else {
+        if(*m_ratio - 1)        //minimal ratio is 1
+            setRatio(*m_ratio - 1);
+    }
+}
+
+void GraphAxis::calculateOffset()
+{
+    const int offset = 10;
+
+    if(m_enabledDirections & GraphAxis::Vertical) {
+        QString intervalMin = QString::number(m_values->min(), 'f', 1);
+        QString intervalMax = QString::number(m_values->max(), 'f', 1);
+        QFont font(m_font, m_fontSize);
+        QFontMetrics fm(font);
+
+        const int textWidth = (intervalMin.length() > intervalMax.length())
+                        ?fm.width(intervalMin)
+                        :fm.width(intervalMax);
+        m_offset = offset + textWidth;
+    }
+
     else if(m_enabledDirections & GraphAxis::Horizontal)
-        paintHorizontalAxis(painter);
+        m_offset = offset + m_fontSize;
+    emit offsetChanged(m_offset);
+}
+
+void GraphAxis::setRatioBinding(int direction)
+{
+    if(direction & GraphAxis::Vertical)
+        m_ratio = &m_verticalRatio;
+    else if(direction & GraphAxis::Horizontal)
+        m_ratio = &m_horizontalRatio;
 }
 
 QString GraphAxis::font() const
@@ -87,6 +134,16 @@ int GraphAxis::lineWidth() const
 int GraphAxis::fontSize() const
 {
     return m_fontSize;
+}
+
+int GraphAxis::offset() const
+{
+    return m_offset;
+}
+
+double GraphAxis::ratio() const
+{
+    return *m_ratio;
 }
 
 void GraphAxis::setFont(QString font)
@@ -132,4 +189,17 @@ void GraphAxis::setFontSize(int fontSize)
 
     m_fontSize = fontSize;
     emit fontSizeChanged(fontSize);
+}
+
+
+void GraphAxis::setRatio(double ratio)
+{
+    if (*m_ratio == ratio)
+        return;
+
+    if(m_enabledDirections & GraphAxis::Vertical)
+        setVerticalRatio(ratio);
+    else if(m_enabledDirections & GraphAxis::Horizontal)
+        setHorizontalRatio(ratio);
+    emit ratioChanged(ratio);
 }
--- types/controls/visualization/graphaxis.h
+++ types/controls/visualization/graphaxis.h
@@ -2,6 +2,7 @@
 #define GraphAxis_H
 
 #include "abstractgraphaxis.h"
+#include <functional>
 
 class GraphAxis : public AbstractGraphAxis
 {
@@ -11,6 +12,8 @@ class GraphAxis : public AbstractGraphAxis
         Q_PROPERTY(QString font READ font WRITE setFont NOTIFY fontChanged)
         Q_PROPERTY(int lineWidth READ lineWidth WRITE setLineWidth NOTIFY lineWidthChanged)
         Q_PROPERTY(int fontSize READ fontSize WRITE setFontSize NOTIFY fontSizeChanged)
+        Q_PROPERTY(int offset READ offset NOTIFY offsetChanged)
+        Q_PROPERTY(double ratio READ ratio WRITE setRatio NOTIFY ratioChanged)
 
     private:
         QString m_font;
@@ -18,20 +21,35 @@ class GraphAxis : public AbstractGraphAxis
         QColor m_color;
         int m_lineWidth;
         int m_fontSize;
+        int m_offset;
+        double *m_ratio;
 
-        void paintHorizontalAxis(QPainter *painter);
-        void paintVerticalAxis(QPainter *painter);
+        struct GraphPaintProperties{
+            int count;
+            double piece;
+            std::function<QLineF(int)> calculateLinePos;
+            std::function<QPointF(int)> calculateTextPos;
+            std::function<QString(int)> getText;
+        };
 
     public:
         GraphAxis();
+        ~GraphAxis();
 
         virtual void paint(QPainter *painter);
+        virtual void wheelEvent(QWheelEvent *e);
 
         QString font() const;
         QColor textColor() const;
         QColor color() const;
         int lineWidth() const;
         int fontSize() const;
+        int offset() const;
+        double ratio() const;
+
+    private slots:
+        void calculateOffset();
+        void setRatioBinding(int direction);
 
     public slots:
         void setFont(QString font);
@@ -39,6 +57,7 @@ class GraphAxis : public AbstractGraphAxis
         void setColor(QColor color);
         void setLineWidth(int lineWidth);
         void setFontSize(int fontSize);
+        void setRatio(double ratio);
 
     signals:
         void fontChanged(QString font);
@@ -46,6 +65,8 @@ class GraphAxis : public AbstractGraphAxis
         void colorChanged(QColor color);
         void lineWidthChanged(int lineWidth);
         void fontSizeChanged(int fontSize);
+        void ratioChanged(double ratio);
+        void offsetChanged(int);
 };
 
 #endif // GraphAxis_H
