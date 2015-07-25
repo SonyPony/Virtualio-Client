91e0d0b992f3805a1c39e9e8147d7fa2c87a339c
Sony Nguyen <sirarseus@gmail.com>
2015-07-25 18:33:11 +0200

    Add C++: add Axis properties which holds GraphAxis objects for calculating position - AbstractGraphContent

--- types/controls/visualization/abstractgraphcontent.cpp
+++ types/controls/visualization/abstractgraphcontent.cpp
@@ -1,8 +1,8 @@
 #include "AbstractGraphcontent.h"
+#include <QPainter>
 
-AbstractGraphContent::AbstractGraphContent(): ResizeAbleItem()
+AbstractGraphContent::AbstractGraphContent(): ResizeAbleItem(), m_verticalAxis(NULL), m_horizontalAxis(NULL)
 {
-
 }
 
 QList<double> AbstractGraphContent::dataX() const
@@ -13,6 +13,26 @@ QList<double> AbstractGraphContent::dataX() const
 QList<double> AbstractGraphContent::dataY() const
 {
     return m_dataY;
+}
+
+GraphAxis *AbstractGraphContent::verticalAxis() const
+{
+    return m_verticalAxis;
+}
+
+GraphAxis *AbstractGraphContent::horizontalAxis() const
+{
+    return m_horizontalAxis;
+}
+
+void AbstractGraphContent::setHeightSameAsAxis()
+{
+    setHeight(m_verticalAxis->height());
+}
+
+void AbstractGraphContent::setWidthSameAsAxis()
+{
+    setWidth(m_horizontalAxis->width());
 }
 
 void AbstractGraphContent::setDataX(QList<double> dataX)
@@ -31,5 +51,36 @@ void AbstractGraphContent::setDataY(QList<double> dataY)
 
     m_dataY = dataY;
     emit dataYChanged(dataY);
+}
+
+void AbstractGraphContent::setVerticalAxis(GraphAxis *verticalAxis)
+{
+    if (m_verticalAxis == verticalAxis)
+        return;
+
+    if(m_verticalAxis) {
+    disconnect(m_verticalAxis, SIGNAL(offsetChanged(int)), this, SLOT(setXSameAsAxis()));
+    disconnect(m_verticalAxis, SIGNAL(heightChanged()), this, SLOT(setHeightSameAsAxis()));
+}
+    m_verticalAxis = verticalAxis;
+
+    setHeightSameAsAxis();
+    connect(m_verticalAxis, SIGNAL(heightChanged()), this, SLOT(setHeightSameAsAxis()));
+    emit verticalAxisChanged(verticalAxis);
+}
+
+void AbstractGraphContent::setHorizontalAxis(GraphAxis *horizontalAxis)
+{
+    if (m_horizontalAxis == horizontalAxis)
+        return;
+
+    if(m_horizontalAxis)
+        disconnect(m_horizontalAxis, SIGNAL(widthChanged()), this, SLOT(setWidthSameAsAxis()));
+
+    m_horizontalAxis = horizontalAxis;
+
+    setWidthSameAsAxis();
+    connect(m_horizontalAxis, SIGNAL(widthChanged()), this, SLOT(setWidthSameAsAxis()));
+    emit horizontalAxisChanged(horizontalAxis);
 }
 
--- types/controls/visualization/abstractgraphcontent.h
+++ types/controls/visualization/abstractgraphcontent.h
@@ -3,16 +3,21 @@
 
 #include "../resizeableitem.h"
 #include "core/interval.h"
+#include "graphaxis.h"
 
 class AbstractGraphContent : public ResizeAbleItem
 {
         Q_OBJECT
         Q_PROPERTY(QList<double> dataX READ dataX WRITE setDataX NOTIFY dataXChanged)
         Q_PROPERTY(QList<double> dataY READ dataY WRITE setDataY NOTIFY dataYChanged)
+        Q_PROPERTY(GraphAxis* verticalAxis READ verticalAxis WRITE setVerticalAxis NOTIFY verticalAxisChanged)
+        Q_PROPERTY(GraphAxis* horizontalAxis READ horizontalAxis WRITE setHorizontalAxis NOTIFY horizontalAxisChanged)
 
     protected:
         QList<double> m_dataX;
         QList<double> m_dataY;
+        GraphAxis* m_verticalAxis;
+        GraphAxis* m_horizontalAxis;
 
     public:
         AbstractGraphContent();
@@ -21,14 +26,25 @@ class AbstractGraphContent : public ResizeAbleItem
 
         QList<double> dataX() const;
         QList<double> dataY() const;
+        GraphAxis* verticalAxis() const;
+        GraphAxis* horizontalAxis() const;
+
+    private slots:
+        void setHeightSameAsAxis();
+        void setWidthSameAsAxis();
 
     public slots:
         void setDataX(QList<double> dataX);
         void setDataY(QList<double> dataY);
 
+        void setVerticalAxis(GraphAxis* verticalAxis);
+        void setHorizontalAxis(GraphAxis* horizontalAxis);
+
     signals:
         void dataXChanged(QList<double> dataX);
         void dataYChanged(QList<double> dataY);
+        void verticalAxisChanged(GraphAxis* verticalAxis);
+        void horizontalAxisChanged(GraphAxis* horizontalAxis);
 };
 
 #endif // AbstractGraphContent_H
