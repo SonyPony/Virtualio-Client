59e354bfd5b644794be32b95c608710030a65465
Sony Nguyen <sirarseus@gmail.com>
2015-07-25 18:34:34 +0200

    Add C++: whole interface and paint method - GraphContent

--- types/controls/visualization/graphcontent.cpp
+++ types/controls/visualization/graphcontent.cpp
@@ -1,8 +1,111 @@
 #include "graphcontent.h"
+#include <QPainterPath>
+
+QPoint GraphContent::combinePoints(QPoint verAxis, QPoint horAxis)
+{
+    return QPoint(horAxis.x(), verAxis.y());
+}
 
 GraphContent::GraphContent(): AbstractGraphContent()
 {
+    connect(this, SIGNAL(contentYChanged(int)), this, SLOT(update()));
+    connect(m_horizontalAxis, SIGNAL(propertiesReady()), this, SLOT(update()));
+    connect(m_verticalAxis, SIGNAL(propertiesReady()), this, SLOT(update()));
+}
 
+void GraphContent::paint(QPainter *painter)
+{
+    if(m_dataX.length() != m_dataY.length())    //something messed up
+        return;
+
+    //DRAW GRAPH POINTS
+    QPainterPath path;
+    QPainterPath fillPath;
+    QPoint point;
+    double markSquareSize = 6;
+    QPointF firstPoint = QPointF(combinePoints(
+                             m_verticalAxis->calculatePointPos(m_dataY[0]),
+                             m_horizontalAxis->calculatePointPos(m_dataX[0])));
+    path.moveTo(firstPoint);      //start at first point
+
+    for(int i = 0; i < m_dataX.length(); i++) {
+        point = combinePoints(
+                    m_verticalAxis->calculatePointPos(m_dataY[i]),
+                    m_horizontalAxis->calculatePointPos(m_dataX[i]));
+        path.lineTo(QPointF(point));
+        painter->fillRect(QRectF(point.x() - markSquareSize / 2., m_contentY, markSquareSize, markSquareSize), QBrush(m_color));
+    }
+
+    //close polygon
+    fillPath = path;
+    fillPath.lineTo(point.x(), height());
+    fillPath.lineTo(firstPoint.x(), height());
+    fillPath.lineTo(firstPoint);
+
+    painter->setPen(QPen(m_color, m_lineWidth));
+    painter->setRenderHints(QPainter::Antialiasing);
+    painter->drawPath(path);
+    painter->fillPath(fillPath, QBrush(QColor(
+                                           m_color.red(),
+                                           m_color.green(),
+                                           m_color.blue(),
+                                           50)));
+}
+
+int GraphContent::lineWidth() const
+{
+    return m_lineWidth;
+}
+
+QColor GraphContent::color() const
+{
+    return m_color;
+}
+
+QColor GraphContent::fillColor() const
+{
+    return m_fillColor;
+}
+
+int GraphContent::contentY() const
+{
+    return m_contentY;
+}
+
+void GraphContent::setLineWidth(int lineWidth)
+{
+    if (m_lineWidth == lineWidth)
+        return;
+
+    m_lineWidth = lineWidth;
+    emit lineWidthChanged(lineWidth);
+}
+
+void GraphContent::setColor(QColor color)
+{
+    if (m_color == color)
+        return;
+
+    m_color = color;
+    emit colorChanged(color);
+}
+
+void GraphContent::setFillColor(QColor fillColor)
+{
+    if (m_fillColor == fillColor)
+        return;
+
+    m_fillColor = fillColor;
+    emit fillColorChanged(fillColor);
+}
+
+void GraphContent::setContentY(int contentY)
+{
+    if (m_contentY == contentY)
+        return;
+
+    m_contentY = contentY;
+    emit contentYChanged(contentY);
 }
 
 
--- types/controls/visualization/graphcontent.h
+++ types/controls/visualization/graphcontent.h
@@ -6,15 +6,40 @@
 class GraphContent : public AbstractGraphContent
 {
         Q_OBJECT
+        Q_PROPERTY(QColor color READ color WRITE setColor NOTIFY colorChanged)
+        Q_PROPERTY(QColor fillColor READ fillColor WRITE setFillColor NOTIFY fillColorChanged)
+        Q_PROPERTY(int lineWidth READ lineWidth WRITE setLineWidth NOTIFY lineWidthChanged)
+        Q_PROPERTY(int contentY READ contentY WRITE setContentY NOTIFY contentYChanged)
 
     private:
+        int m_lineWidth;
+        QColor m_color;
+        QColor m_fillColor;
+        int m_contentY;
+
+        inline QPoint combinePoints(QPoint verAxis, QPoint horAxis);
 
     public:
         GraphContent();
 
-    signals:
+        virtual void paint(QPainter* painter);
+
+        int lineWidth() const;
+        QColor color() const;
+        QColor fillColor() const;
+        int contentY() const;
 
     public slots:
+        void setLineWidth(int lineWidth);
+        void setColor(QColor color);
+        void setFillColor(QColor fillColor);
+        void setContentY(int contentY);
+
+    signals:
+        void lineWidthChanged(int lineWidth);
+        void colorChanged(QColor color);
+        void fillColorChanged(QColor fillColor);
+        void contentYChanged(int contentY);
 };
 
 #endif // GRAPHCONTENT_H
