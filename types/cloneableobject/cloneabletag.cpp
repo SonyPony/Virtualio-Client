eb5317804ccc0cb8b85fe17d131f082be1add5c0
Sony Nguyen <sirarseus@gmail.com>
2015-08-24 21:43:47 +0200

    Add C++: CloneableTag contains pointer to DropGrid to register cloned objects

--- types/cloneableobject/cloneabletag.cpp
+++ types/cloneableobject/cloneabletag.cpp
@@ -12,9 +12,24 @@ void CloneableTag::paint(QPainter *painter)
     painter->drawRect(boundingRect());
 }
 
-void CloneableTag::mousePressEvent(QMouseEvent *event)
+void CloneableTag::mousePressEvent(QMouseEvent *)
 {
     QPointer<CloneTag> instance = m_manager->clone(this);
+    m_dropGrid->registerObject(instance);
     instance->grabMouse();
+}
+
+DropGrid *CloneableTag::dropGrid() const
+{
+    return m_dropGrid;
+}
+
+void CloneableTag::setDropGrid(DropGrid *dropGrid)
+{
+    if (m_dropGrid == dropGrid)
+        return;
+
+    m_dropGrid = dropGrid;
+    emit dropGridChanged(dropGrid);
 }
 
--- types/cloneableobject/cloneabletag.h
+++ types/cloneableobject/cloneabletag.h
@@ -4,24 +4,32 @@
 #include "clonemanager.h"
 #include "clonetag.h"
 #include "../../core/painteditem.h"
+#include "../layout/dropgrid.h"
 #include <QQuickPaintedItem>
 #include <QPainter>
 
 class CloneableTag : public PaintedItem
 {
         Q_OBJECT
+        Q_PROPERTY(DropGrid* dropGrid READ dropGrid WRITE setDropGrid NOTIFY dropGridChanged)
+
     protected:
         CloneManager *m_manager;
+        DropGrid* m_dropGrid;
 
     public:
         CloneableTag();
 
         virtual void paint(QPainter* painter);
-        virtual void mousePressEvent(QMouseEvent* event);
+        virtual void mousePressEvent(QMouseEvent*);
 
-    signals:
+        DropGrid* dropGrid() const;
 
     public slots:
+        void setDropGrid(DropGrid* dropGrid);
+
+    signals:
+        void dropGridChanged(DropGrid* dropGrid);
 };
 
 #endif // CLONEABLETAG_H
