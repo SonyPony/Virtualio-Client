a7ab69f9c0eac3ea4b42ddc0b0c54a5d4a3add54
Sony Nguyen <sirarseus@gmail.com>
2015-11-01 14:58:52 +0100

    Change C++: extends DragAndDropManager functionalities - supports managing one axis dragging, QPoint can be used instead of QMouseEvent, dinstalFromTLCorner getter

--- src/common/draganddropmanager.cpp
+++ src/common/draganddropmanager.cpp
@@ -1,21 +1,45 @@
 #include "draganddropmanager.h"
+#include <QPoint>
 
 DragAndDropManager::DragAndDropManager(QQuickItem *component, QObject *parent) : QObject(parent)
 {
+    m_enabledOrientations = Qt::Horizontal | Qt::Vertical;
     m_pressed = false;
     m_target = component;
 }
 
+QSize DragAndDropManager::distanceFromTLCorner() const
+{
+    return m_distanceFromTLCorner;
+}
+
+void DragAndDropManager::setEnabledOrientations(int orientations)
+{
+    m_enabledOrientations = orientations;
+}
+
 void DragAndDropManager::move(QMouseEvent *event)
 {
-    m_target->setX(event->x() + m_target->x() - m_distanceFromTLCorner.width());
-    m_target->setY(event->y() + m_target->y() - m_distanceFromTLCorner.height());
+    this->move(event->pos());
+}
+
+void DragAndDropManager::move(QPoint diffPoint)
+{
+    if(m_enabledOrientations & Qt::Horizontal)
+        m_target->setX(diffPoint.x() + m_target->x() - m_distanceFromTLCorner.width());
+    if(m_enabledOrientations & Qt::Vertical)
+        m_target->setY(diffPoint.y() + m_target->y() - m_distanceFromTLCorner.height());
+}
+
+void DragAndDropManager::pressed(QPoint diffPoint)
+{
+    m_distanceFromTLCorner = QSize(diffPoint.x(), diffPoint.y());
+    m_pressed = true;
 }
 
 void DragAndDropManager::pressed(QMouseEvent *event)
 {
-    m_distanceFromTLCorner = QSize(event->x(), event->y());
-    m_pressed = true;
+    this->pressed(event->pos());
 }
 
 void DragAndDropManager::released()
--- src/common/draganddropmanager.h
+++ src/common/draganddropmanager.h
@@ -12,12 +12,19 @@ class DragAndDropManager : public QObject
         QQuickItem* m_target;
         bool m_pressed;
         QSize m_distanceFromTLCorner;
+        int m_enabledOrientations;
 
     public:
         DragAndDropManager(QQuickItem* component, QObject *parent = 0);
 
+        QSize distanceFromTLCorner() const;
+        void setEnabledOrientations(int orientations);
+        // can be used only if it's called from component::mouseMoveEvent
         void move(QMouseEvent* event);
+        void move(QPoint diffPoint);
+        // can be used only if it's called from component::mousePressEvent
         void pressed(QMouseEvent* event);
+        void pressed(QPoint diffPoint);
         void released();
 };
 
