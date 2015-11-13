3b626504daa44ad4a2d23bad3eff4dce016e2a69
Sony Nguyen <sirarseus@gmail.com>
2015-11-13 02:43:55 +0100

    Add C++: ScrollView colors

--- src/types/containers/scrollview.cpp
+++ src/types/containers/scrollview.cpp
@@ -9,30 +9,39 @@ ScrollView::ScrollView(QQuickItem *parent): PaintedItem(parent)
     m_horizontalScrollBar->setOrientation(Qt::Horizontal);
     m_verticalScrollBar->setOrientation(Qt::Vertical);
 
-    m_horizontalScrollBar->setColor("red");
-    m_verticalScrollBar->setColor("red");
+    m_horizontalScrollBar->setZ(1);
+    m_verticalScrollBar->setZ(1);
 
-    m_horizontalScrollBar->setBackgroundColor("lime");
-    m_verticalScrollBar->setBackgroundColor("lime");
+    connect(this, &ScrollView::widthChanged, this, &ScrollView::checkIfHideScrollBars);
+    connect(this, &ScrollView::heightChanged, this, &ScrollView::checkIfHideScrollBars);
+    // set colors
+    connect(this, &ScrollView::colorChanged, m_verticalScrollBar, &ScrollBar::setColor);
+    connect(this, &ScrollView::colorChanged, m_horizontalScrollBar, &ScrollBar::setColor);
+    connect(this, &ScrollView::backgroundColorChanged, m_horizontalScrollBar, &ScrollBar::setBackgroundColor);
+    connect(this, &ScrollView::backgroundColorChanged, m_verticalScrollBar, &ScrollBar::setBackgroundColor);
 
-    connect(this, &QQuickItem::widthChanged, [this]() {
+    // update scrollbar size
+    connect(this, &ScrollView::widthChanged, [this]() {
         m_horizontalScrollBar->setWidth(this->width());
         m_verticalScrollBar->setWidth(6);
         updateHorizontalScrollbar();
     });
     
-    connect(this, &QQuickItem::heightChanged, [this]() {
+    // update scrollbar size
+    connect(this, &ScrollView::heightChanged, [this]() {
         m_horizontalScrollBar->setHeight(6);
         m_verticalScrollBar->setHeight(this->height());
         updateVerticalScrollbar();
     });
     
-    connect(this, &QQuickItem::childrenChanged, [this]() {
+    // register last children, because scrollbars are first 2 childrens
+    connect(this, &ScrollView::childrenChanged, [this]() {
         this->registerItem(this->childItems().last());
         updateVerticalScrollbar();
         updateHorizontalScrollbar();
     });
 
+    // move horizontally
     connect(m_horizontalScrollBar, &ScrollBar::positionChanged, [this](qreal pos) {
         if(m_item == NULL)
             return;
@@ -40,6 +49,7 @@ ScrollView::ScrollView(QQuickItem *parent): PaintedItem(parent)
         m_item->setX(-pos * m_item->width());
     });
 
+    // move vertically
     connect(m_verticalScrollBar, &ScrollBar::positionChanged, [this](qreal pos) {
         if(m_item == NULL)
             return;
@@ -53,10 +63,42 @@ void ScrollView::paint(QPainter *painter)
 
 }
 
+QColor ScrollView::backgroundColor() const
+{
+    return m_backgroundColor;
+}
+
+void ScrollView::setBackgroundColor(QColor backgroundColor)
+{
+    if (m_backgroundColor == backgroundColor)
+        return;
+
+    m_backgroundColor = backgroundColor;
+    Q_EMIT backgroundColorChanged(backgroundColor);
+}
+
+void ScrollView::checkIfHideScrollBars()
+{
+    if(m_item == NULL)
+        return;
+
+    if(width() >= m_item->width())  // if scrollview is bigger than item, then hide scrollbars
+        m_horizontalScrollBar->setOpacity(0);
+    else if(!m_horizontalScrollBar->opacity())  // otherwise hide if is hidden
+        m_horizontalScrollBar->setOpacity(1);
+
+    if(height() >= m_item->height())  // if scrollview is bigger than item, then hide scrollbars
+        m_verticalScrollBar->setOpacity(0);
+    else if(!m_verticalScrollBar->opacity())  // otherwise hide if is hidden
+        m_verticalScrollBar->setOpacity(1);
+}
+
 void ScrollView::registerItem(QQuickItem *item)
 {
-    disconnect(m_item, &QQuickItem::widthChanged, this, &ScrollView::updateHorizontalScrollbar);
-    disconnect(m_item, &QQuickItem::heightChanged, this, &ScrollView::updateVerticalScrollbar);
+    if(m_item) {
+        disconnect(m_item, &QQuickItem::widthChanged, this, &ScrollView::updateHorizontalScrollbar);
+        disconnect(m_item, &QQuickItem::heightChanged, this, &ScrollView::updateVerticalScrollbar);
+    }
 
     m_item = item;
     connect(m_item, &QQuickItem::widthChanged, this, &ScrollView::updateHorizontalScrollbar);
@@ -67,7 +109,7 @@ void ScrollView::updateVerticalScrollbar()
 {
     if(m_item == NULL)
         return;
-qDebug() << height() / m_item->height();
+
     m_verticalScrollBar->setRatio(m_item->height() / height());
 }
 
@@ -75,7 +117,7 @@ void ScrollView::updateHorizontalScrollbar()
 {
     if(m_item == NULL)
         return;
-qDebug() << width() / m_item->width();
+
     m_horizontalScrollBar->setRatio(m_item->width() / width());
 }
 
--- src/types/containers/scrollview.h
+++ src/types/containers/scrollview.h
@@ -8,24 +8,33 @@
 class ScrollView : public PaintedItem
 {
         Q_OBJECT
+        Q_PROPERTY(QColor backgroundColor READ backgroundColor WRITE setBackgroundColor NOTIFY backgroundColorChanged)
 
     private:
         QQuickItem* m_item;
         ScrollBar* m_horizontalScrollBar;
         ScrollBar* m_verticalScrollBar;
+        QColor m_backgroundColor;
 
     public:
         ScrollView(QQuickItem* parent = 0);
 
         virtual void paint(QPainter* painter);
 
+        QColor backgroundColor() const;
+
+    public Q_SLOTS:
+        void setBackgroundColor(QColor backgroundColor);
+
     private Q_SLOTS:
+        void checkIfHideScrollBars();
         void registerItem(QQuickItem* item);
         void updateVerticalScrollbar();
         void updateHorizontalScrollbar();
 
     Q_SIGNALS:
         void newItem(QQuickItem* item);
+        void backgroundColorChanged(QColor backgroundColor);
 };
 
 #endif // SCROLLVIEW_H
