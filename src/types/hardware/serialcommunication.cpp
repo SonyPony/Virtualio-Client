851d69e0dc76703bdc1eb9d65a1ab35f7205503c
Sony Nguyen <sirarseus@gmail.com>
2016-02-05 10:29:19 +0100

    Add C++: SerialCommunication - extend functionality -> coinnection state, set msg separator, com potrs infos

--- src/types/hardware/serialcommunication.cpp
+++ src/types/hardware/serialcommunication.cpp
@@ -2,9 +2,20 @@
 
 SerialCommunication::SerialCommunication()
 {
+    m_timer = new QTimer(this);
+    m_timer->setSingleShot(false);
+    m_timer->setInterval(150);
+    m_separator = QString(" ");
+
+    m_portIdentifier = "";
+    m_portName = "";
     m_messageBuffer = "";
+    m_connectionState = SerialCommunication::Disconnected;
     m_connected = false;
-    QObject::connect(this, SIGNAL(found()), this, SLOT(setPort()));
+    m_comPortsInfo = QStringList();
+
+    connect(this, &SerialCommunication::found, this, &SerialCommunication::setPort);
+    connect(m_timer, &QTimer::timeout, this, &SerialCommunication::updatePortsInfo);
 }
 
 SerialCommunication::~SerialCommunication() {}
@@ -24,17 +35,53 @@ bool SerialCommunication::connected() const
     return m_connected;
 }
 
+int SerialCommunication::connectionState() const
+{
+    return m_connectionState;
+}
+
+QStringList SerialCommunication::comPortsInfo() const
+{
+    return m_comPortsInfo;
+}
+
+QString SerialCommunication::separator() const
+{
+    return m_separator;
+}
+
+void SerialCommunication::updatePortsInfo()
+{
+    QString comPortInfo;
+    QStringList comPorts;
+
+    for(const QSerialPortInfo singlePort: QSerialPortInfo::availablePorts()) {
+        /*comPortInfo = QString("%1: Manufacturer: %2 Description: %3")
+                .arg(singlePort.portName())
+                .arg(singlePort.manufacturer())
+                .arg(singlePort.description());*/
+        comPortInfo = QString("%1")
+                .arg(singlePort.portName());
+        comPorts.append(comPortInfo);
+    }
+
+    if(m_comPortsInfo != comPorts) {
+        m_comPortsInfo = comPorts;
+        Q_EMIT this->comPortsInfoChanged(m_comPortsInfo);
+    }
+}
+
 void SerialCommunication::setPort()
 {
     m_serial = new QSerialPort(m_serialInfo);
-    m_serial->setBaudRate(QSerialPort::Baud115200);
+    m_serial->setBaudRate(QSerialPort::Baud9600);
     m_serial->setParity(QSerialPort::NoParity);
     m_serial->setStopBits(QSerialPort::OneStop);
     m_serial->setDataBits(QSerialPort::Data8);
     m_serial->setFlowControl(QSerialPort::NoFlowControl);
 
     if(!m_serial->open(QIODevice::ReadWrite))
-        emit error("Unable to connect.");
+        emit error(tr("Unable to connect."));
 
     QObject::connect(m_serial, SIGNAL(readyRead()), this, SLOT(handleReadyRead()));
 }
@@ -42,15 +89,34 @@ void SerialCommunication::setPort()
 void SerialCommunication::handleReadyRead()
 {
     m_messageBuffer += QString(m_serial->readAll());
-    QStringList separatedData;
-    if(m_messageBuffer.contains(QString(";"), Qt::CaseInsensitive)) {
-        separatedData = m_messageBuffer.split(";", QString::KeepEmptyParts, Qt::CaseInsensitive);
 
+    QStringList separatedData;
+    if(m_messageBuffer.contains(m_separator, Qt::CaseSensitive)) {
+        separatedData = m_messageBuffer.split(m_separator, QString::KeepEmptyParts, Qt::CaseSensitive);
         emit message(separatedData.at(0));
 
         if(separatedData.length() > 1)
             m_messageBuffer = separatedData.at(1);
     }
+}
+
+void SerialCommunication::setConnectionState(int arg)
+{
+    if(m_connectionState == arg)
+        return;
+
+    m_connectionState = arg;
+    Q_EMIT this->connectionStateChanged(arg);
+}
+
+void SerialCommunication::startPeriodicUpdate()
+{
+    m_timer->start();
+}
+
+void SerialCommunication::stopPeriodicUpdate()
+{
+    m_timer->stop();
 }
 
 void SerialCommunication::setPortIdentifier(QString arg)
@@ -73,18 +139,39 @@ void SerialCommunication::setPortName(QString arg)
 
 
 void SerialCommunication::tryConnect()
-{
+{   
+    this->updatePortsInfo();
+    bool found = false;
+    QSerialPortInfo portInfo;
+
     for(const QSerialPortInfo singlePort: QSerialPortInfo::availablePorts()) {
-        if(singlePort.description().contains(m_portIdentifier, Qt::CaseInsensitive)) {
-            m_serialInfo = singlePort;
-            qDebug() << m_serialInfo.portName();
-            setConnected(true);
-            emit found();
-            return;
+        if(!m_portIdentifier.isEmpty()) {
+            if(singlePort.description().contains(m_portIdentifier, Qt::CaseSensitive)) {
+                portInfo = singlePort;
+                found = true;
+                break;
+            }
+        }
+
+        else if(!m_portName.isEmpty()) {
+            if(singlePort.portName() == m_portName) {
+                portInfo = singlePort;
+                found = true;
+                break;
+            }
         }
     }
 
-    emit error("Unable to connect.");
+    if(found) {
+        m_serialInfo = portInfo;
+        qDebug() << "Found:" << m_serialInfo.portName();
+        this->setConnected(true);
+        Q_EMIT this->found();
+        return;
+    }
+
+
+    Q_EMIT this->error("Unable to connect.");
 }
 
 void SerialCommunication::setConnected(bool arg)
@@ -92,6 +179,20 @@ void SerialCommunication::setConnected(bool arg)
     if (m_connected == arg)
         return;
 
+    if(arg)
+        this->setConnectionState(SerialCommunication::WaitingOnResponse);
+    else
+        this->setConnectionState(SerialCommunication::Disconnected);
+
     m_connected = arg;
     emit connectedChanged(arg);
+}
+
+void SerialCommunication::setSeparator(QString separator)
+{
+    if (m_separator == separator)
+        return;
+
+    m_separator = separator;
+    emit separatorChanged(separator);
 }
--- src/types/hardware/serialcommunication.h
+++ src/types/hardware/serialcommunication.h
@@ -4,41 +4,66 @@
 #include <QQuickItem>
 #include <QSerialPort>
 #include <QSerialPortInfo>
+#include <QTimer>
 
 class SerialCommunication : public QQuickItem
 {
-    Q_OBJECT
+        Q_OBJECT
+        Q_ENUMS(ConnectionState)
         Q_PROPERTY(QString portIdentifier READ portIdentifier WRITE setPortIdentifier NOTIFY portIdentifierChanged)
         Q_PROPERTY(QString portName READ portName WRITE setPortName NOTIFY portNameChanged)
         Q_PROPERTY(bool connected READ connected WRITE setConnected NOTIFY connectedChanged)
+        Q_PROPERTY(int connectionState READ connectionState NOTIFY connectionStateChanged)
+        Q_PROPERTY(QStringList comPortsInfo READ comPortsInfo NOTIFY comPortsInfoChanged)
+        Q_PROPERTY(QString separator READ separator WRITE setSeparator NOTIFY separatorChanged)
 
     private:
+        QTimer* m_timer;
         QString m_portIdentifier;
         QString m_portName;
         QSerialPort *m_serial;
         QSerialPortInfo m_serialInfo;
         bool m_connected;
         QString m_messageBuffer;
+        int m_connectionState;
+        QStringList m_comPortsInfo;
+        QString m_separator;
 
     public:
+        enum ConnectionState {
+            WaitingOnResponse,
+            Connected,
+            Disconnected
+        };
+
         SerialCommunication();
         ~SerialCommunication();
 
         QString portIdentifier() const;
         QString portName() const;
         bool connected() const;
+        int connectionState() const;
+        QStringList comPortsInfo() const;
+        QString separator() const;
 
-    private slots:
+    private Q_SLOTS:
         void setPort();
         void handleReadyRead();
 
-    public slots:
+    protected Q_SLOTS:
+        void setConnectionState(int arg);
+
+    public Q_SLOTS:
+        void startPeriodicUpdate();
+        void stopPeriodicUpdate();
+        void updatePortsInfo();
         void setPortIdentifier(QString arg);
         void setPortName(QString arg);
         void tryConnect();
         void setConnected(bool arg);
+        void setSeparator(QString separator);
 
-    signals:
+    Q_SIGNALS:
         void message(QString arg);
         void found();
         void error(QString arg);
@@ -46,6 +71,9 @@ class SerialCommunication : public QQuickItem
         void portIdentifierChanged(QString arg);
         void portNameChanged(QString arg);
         void connectedChanged(bool arg);
+        void connectionStateChanged(int connectionState);
+        void comPortsInfoChanged(QStringList comPortsInfo);
+        void separatorChanged(QString separator);
 };
 
 #endif // SERIALCOMMUNICATION_H
